1ebf22629e6c1d43778b57a14fd6dcb604255cd7
vilascognizantnew <72960946+vilascognizantnew@users.noreply.github.com>
2020-10-23 18:13:37 +0530

    Update ServletInitializer.java

--- RestaurantApplication/src/main/java/com/cts/avijeet/ServletInitializer.java
+++ RestaurantApplication/src/main/java/com/cts/avijeet/ServletInitializer.java
@@ -1,5 +1,11 @@
 package com.cts.avijeet;
 
-public class ServletInitializer {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+public class ServletInitializer extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestaurantApplication.class);
+	}
 }
