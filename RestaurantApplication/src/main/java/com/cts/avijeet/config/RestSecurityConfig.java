048447b99f88f86e03e947e0f412947f44f23a71
vilascognizantnew <72960946+vilascognizantnew@users.noreply.github.com>
2020-10-23 18:16:09 +0530

    Update RestSecurityConfig.java

--- RestaurantApplication/src/main/java/com/cts/avijeet/config/RestSecurityConfig.java
+++ RestaurantApplication/src/main/java/com/cts/avijeet/config/RestSecurityConfig.java
@@ -1,5 +1,37 @@
 package com.cts.avijeet.config;
 
-public class RestSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+		.csrf()
+		.disable()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.GET,"/stock/**").hasAnyRole("USER")
+		.antMatchers(HttpMethod.POST,"/stock/**").hasAnyRole("ADMIN")
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("user")
+			.password("{noop}user")
+			.roles("USER")
+			.and()
+			.withUser("admin")
+			.password("{noop}admin")
+			.roles("ADMIN");
+	}
 }
